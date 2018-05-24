fa1abf61ae87573eae72a91a03c421a975525d35
Nikita <messi3nik@gmail.com>
2018-05-25 01:56:28 +0300

    verrryy cool

---
+++ main/scala/Laba.scala
@@ -0,0 +1,122 @@
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
+
+object Laba extends App {
+  //first
+  def nElementRecur(list: List[Int], index: Int): Int = {
+    if (list.length >= index) {
+      list(index)
+    }
+    else 0
+  }
+
+  def nElement(list: List[Int], index: Int): Int = {
+    @tailrec
+    def nEle(list: List[Int], index: Int, nElem: Int): Int = {
+      if (nElem == index)
+        list(nElem)
+      else nEle(list, index, nElem + 1)
+    }
+
+    nEle(list, index, 0)
+  }
+
+  //second
+  def isSquare(i: Int): Int = {
+    if (math.round(math.sqrt(i)) * math.round(math.sqrt(i)) == i) 1
+    else -1
+  }
+
+  def findSquaresOnTheInterval(startValue: Int, endValue: Int): IndexedSeq[Double] = {
+    def vector =
+      for (i <- startValue to endValue if isSquare(i) == 1) yield math.sqrt(i)
+
+    vector
+  }
+
+  @tailrec
+  def findSquaresOnTheIntervalReq(startValue: Int, endValue: Int, arr: ArrayBuffer[Double]): ArrayBuffer[Double] = {
+    if (startValue > endValue) arr
+    else {
+      if (isSquare(startValue) == 1) arr += math.sqrt(startValue)
+      findSquaresOnTheIntervalReq(startValue + 1, endValue, arr)
+    }
+  }
+
+
+  //SecondLab
+  //1task
+  def findAverageNum(list: List[Int]): Double = {
+    @tailrec
+    def findAN(list: List[Int], index: Int, sum: Int): Int = {
+      if (index == list.length)
+        sum
+      else {
+        findAN(list, index + 1, sum + list(index))
+      }
+    }
+
+    findAN(list, 0, 0) / list.length
+  }
+
+  //2task
+  def foldList(list: List[Int]): List[Int] = {
+    list.foldRight(List[Int]())((a, z) => z.::(a).::(a))
+  }
+
+
+  //3task
+  def findCPP(list: List[String]): List[String] = {
+    list.filter(isCPP)
+  }
+
+  def isCPP(i: String): Boolean = {
+    i.endsWith("++")
+  }
+
+  //ThirdLaba
+  //1task
+  def antiMulticate(list: List[Int]): List[Int] = {
+    list.map((i: Int) => isPrime(i))
+  }
+
+  def isFactor(number: Int, potentialFactor: Int) =
+    number % potentialFactor == 0
+
+  def factors(number: Int) =
+    (1 to number) filter (isFactor(number, _))
+
+  def sum(factors: Seq[Int]) =
+    factors.foldLeft(0)(_ + _)
+
+  def isPrime(number: Int): Int ={
+    if (sum(factors(number)) == number + 1)
+      number * 2
+    else number*3
+  }
+
+
+  //2task
+
+  abstract class number{
+    def unapply():Double{}
+  }
+  case class complexNumber(real: Double, imaginary: Double) extends number {
+    def unapply():Double={
+      math.sqrt(real*real+imaginary*imaginary)
+    }
+  }
+  case class scalarNumber(real:Double) extends number {
+    def unapply():Double={
+      if(real<0) real*(-1)
+      else real
+    }
+  }
+
+  def module(ourClass: number):Double={
+    ourClass match {
+      case complexNumber(_,_) => ourClass.unapply()
+      case scalarNumber(_)=> ourClass.unapply()
+    }
+  }
+}
---
+++ test/scala/MainTest.scala
@@ -0,0 +1,56 @@
+import org.scalatest.FunSuite
+
+import scala.collection.mutable.ArrayBuffer
+
+class MainTest extends FunSuite {
+
+  val list: List[Int] = List(1,2,3,4,5)
+  val list1: List[String] = List("D","C++","f#","Scala")
+  val arr: ArrayBuffer[Double] = ArrayBuffer()
+  /*
+  test("first") {
+    assert(Laba.nElementRecur(list, 2) === 3)
+  }
+
+  test("second"){
+    assert(Laba.findSquaresOnTheInterval(1,4)===ArrayBuffer(1,2))
+  }
+
+  test("third"){
+    assert(Laba.findSquaresOnTheIntervalReq(1,4,arr)===ArrayBuffer(1,2))
+  }
+
+  test("4"){
+    assert(Laba.nElement(list, 2) === 3)
+  }
+*/
+  test("secondLab1"){
+    assert(Laba.findAverageNum(list)===3)
+  }
+
+  test("secondLab2"){
+    assert(Laba.foldList(List(1,2,3))===List(1,1,2,2,3,3))
+  }
+
+  test("secondLab3"){
+    assert(Laba.findCPP(list1)===List("C++"))
+  }
+
+
+
+
+  test("ThirdLab1"){
+    assert(Laba.antiMulticate(List(2,3,4))===List(4,6,12))
+  }
+
+  test("ThirdLab2_1") {
+    val num = new Laba.complexNumber(-10, 10)
+    assert(Laba.module(num) === 14.142135623730951)
+  }
+
+  test("ThirdLab2_2"){
+    val num = new Laba.scalarNumber(10)
+    assert(Laba.module(num)===10)
+  }
+
+}
